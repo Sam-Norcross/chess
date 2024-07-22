84ef47dc46429164b2bc7eb4063670ca423ea201
Sam Norcross <norcross.sam@gmail.com>
2024-07-22 15:15:54 -0600

    File structure updates

---
+++ server/src/main/java/dataaccess/SQLGameDAO.java
@@ -0,0 +1,45 @@
+package dataaccess;
+
+import chess.ChessGame;
+import model.GameData;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class SQLGameDAO {
+
+    public SQLGameDAO() {
+
+    }
+
+    @Override
+    public GameData createGame(String gameName) {
+
+    }
+
+    @Override
+    public int newGameID() {
+
+    }
+
+    @Override
+    public void clearGames() {
+
+    }
+
+    @Override
+    public GameData getGame(int gameID) {
+
+    }
+
+    @Override
+    public void updateGame(int gameID, GameData gameData) {
+
+    }
+
+    @Override
+    public ArrayList<GameData> getGames() {
+
+    }
+
+}
---
+++ server/src/main/java/dataaccess/SQLUserDAO.java
@@ -0,0 +1,45 @@
+package dataaccess;
+
+import model.AuthData;
+import model.UserData;
+
+public class SQLUserDAO {
+
+    public SQLUserDAO() {
+
+    }
+
+    @Override
+    public void createUser(UserData user) {
+
+    }
+
+    @Override
+    public UserData getUser(String username) {
+
+    }
+
+    @Override
+    public void createAuth(AuthData auth) {
+
+    }
+
+    @Override
+    public AuthData getAuth(String authToken) {
+    }
+
+    @Override
+    public void removeAuth(String authToken) {
+    }
+
+
+    @Override
+    public void clearUsers() {
+    }
+
+    @Override
+    public void clearAuth() {
+
+    }
+
+}
