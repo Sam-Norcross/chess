af6e8a8cecd0c3dd9bd70ed2ada5f846dd3d70bb
Sam Norcross <norcross.sam@gmail.com>
2024-07-30 16:52:14 -0600

    Finished unit tests

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,8 +1,10 @@
 package client;
 
+import chess.ChessGame;
 import dataaccess.DataAccessException;
 import model.CreateRequest;
 import model.GameData;
+import model.JoinRequest;
 import model.UserData;
 import org.junit.jupiter.api.*;
 import server.Server;
@@ -104,6 +106,53 @@ public class ServerFacadeTests {
         ArrayList<GameData> games = serverFacade.listGames(authToken);
         assertEquals(10, games.size());
     }
-    
+
+    @Test
+    public void listBadAuth() throws Exception {
+        serverFacade.register(userBob);
+        String authToken = serverFacade.login(userBob).authToken();
+        for (int i = 0; i < 10; i++) {
+            serverFacade.createGame(new CreateRequest(authToken, "Bob's game number " + i));
+        }
+
+        assertThrows(Exception.class, () -> serverFacade.listGames(UUID.randomUUID().toString()));
+    }
+
+    @Test
+    public void joinExistingGame() throws Exception {
+        serverFacade.register(userBob);
+        String authToken = serverFacade.login(userBob).authToken();
+        serverFacade.createGame(new CreateRequest(authToken, "Bob's game"));
+        assertDoesNotThrow(() -> serverFacade.joinGame(new JoinRequest(authToken, 1, ChessGame.TeamColor.WHITE)));
+    }
+
+    @Test
+    public void joinBadGameID() throws Exception {
+        serverFacade.register(userBob);
+        String authToken = serverFacade.login(userBob).authToken();
+        serverFacade.createGame(new CreateRequest(authToken, "Bob's game"));
+        assertThrows(Exception.class, () -> serverFacade.joinGame(new JoinRequest(authToken, 999, ChessGame.TeamColor.WHITE)));
+    }
+
+    @Test
+    public void clearMultiple() throws Exception {
+        UserData userAlice = new UserData("Alice", "67890", "alice@email.com");
+
+        serverFacade.register(userBob);
+        serverFacade.register(userAlice);
+        String authTokenBob = serverFacade.login(userBob).authToken();
+        String authTokenAlice = serverFacade.login(userAlice).authToken();
+
+        for (int i = 0; i < 5; i++) {
+            serverFacade.createGame(new CreateRequest(authTokenBob, "Bob's game number " + i));
+        }
+        for (int i = 0; i < 5; i++) {
+            serverFacade.createGame(new CreateRequest(authTokenAlice, "Alice's game number " + i));
+        }
+
+        assertDoesNotThrow(() -> serverFacade.clear());
+        assertThrows(Exception.class, () -> serverFacade.login(userBob));
+        assertThrows(Exception.class, () -> serverFacade.login(userAlice));
+    }
 
 }
