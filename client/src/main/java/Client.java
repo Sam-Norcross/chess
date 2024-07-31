00844ef585f298f7d7263913ddd56a0e098999bd
Sam Norcross <norcross.sam@gmail.com>
2024-07-30 20:49:34 -0600

    Added basic functionality for all commands in Client class

--- client/src/main/java/Client.java
+++ client/src/main/java/Client.java
@@ -1,41 +1,63 @@
-import model.UserData;
+import chess.ChessGame;
+import model.*;
 import server.ServerFacade;
+
+import java.util.ArrayList;
 
 public class Client {
 
     private final ServerFacade serverFacade;
-    private boolean isLoggedIn;
+    private String authToken;
 
 
     public Client(String url) {
         serverFacade = new ServerFacade(url);
-        isLoggedIn = false;
+        authToken = null;
+
+
+
+
+        try { //TODO--testing only
+            serverFacade.clear();
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
+
+
+
     }
 
     public String eval(String input) {
-        String message = "";
+        try {
+            String[] tokens = input.split(" ");
+            String command = tokens[0];
 
-        String[] tokens = input.split(" ");
-        String command = tokens[0];
-
-        if (command.equals("help")) {
-            return helpString();
-        } else if (command.equals("login")) {
-            try {
-                serverFacade.login(new UserData(tokens[1], tokens[2], tokens[3]));
-                isLoggedIn = true;
-            } catch (Exception ex) {
-                //
+            if (command.equals("help")) {
+                return helpString();
+            } else if (command.equals("register")) {
+                return register(tokens);
+            } else if (command.equals("login")) {
+                return login(tokens);
+            } else if (command.equals("logout")) {
+                return logout();
+            } else if (command.equals("list")) {
+                return listGames();
+            } else if (command.equals("create")) {
+                return createGame(tokens);
+            } else if (command.equals("join")) {
+                serverFacade.joinGame(new JoinRequest(authToken, Integer.parseInt(tokens[1]),
+                                                        ChessGame.TeamColor.valueOf(tokens[2])));
+            } else if (command.equals("quit")) {
+                return "quit";
             }
-        }
-        if (command.equals("quit")) {
-            return "quit";
+        } catch (Exception ex) {
+            return ex.getMessage();
         }
         return null;
     }
 
     private String helpString() {
-        if (!isLoggedIn) {
+        if (authToken == null) {
             return """
                    Possible commands:
                     register <USERNAME> <PASSWORD> <EMAIL> --register a new user
@@ -54,6 +76,56 @@ public class Client {
                     quit --exit the application
                     help --show possible commands
                    """;
+        }
+    }
+
+    public String register(String[] tokens) throws Exception{
+        try {
+            serverFacade.register(new UserData(tokens[1], tokens[2], tokens[3]));
+            return "Successfully registered user " + tokens[1];
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
+    }
+
+    public String login(String[] tokens) throws Exception{
+        try {
+            authToken = serverFacade.login(new UserData(tokens[1], tokens[2], null)).authToken();
+            return "Successfully logged in user " + tokens[1];
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
+    }
+
+    public String logout() throws Exception{
+        try {
+            serverFacade.logout(authToken);
+            authToken = null;
+            return "Successfully logged out!";
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
+    }
+
+    public String listGames() throws Exception{
+        try {
+            ArrayList<GameData> games = serverFacade.listGames(authToken);
+            String gamesString = "";
+            for (GameData gameData : games) {
+                gamesString += gameData.toString() + "\n";
+            }
+            return gamesString;
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
+    }
+
+    public String createGame(String[] tokens) throws Exception{
+        try {
+            int gameID = serverFacade.createGame(new CreateRequest(authToken, tokens[1])).gameID();
+            return "Game successfully created with gameID " + gameID;
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
         }
     }
 
